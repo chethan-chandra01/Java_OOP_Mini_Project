b13039b619fbafcdd16443a34b039ee1ea6bbba0
chethan-chandra01 <chethanmadugula@gmail.com>
2026-01-28 18:34:47 +0530

    Update Main.java

--- Mini project/src/Main.java
+++ Mini project/src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Student{
+    int Id;
+    String Name;
+    String Dept;
+    int year;
+    private double Phone_Number;
+    int Room;
+    int Hostel_Fees ;
+    void Show_Info(){
+        System.out.println("Name :"+" "+this.Name);
+        System.out.println("Department"+" "+this.Dept);
+        System.out.println("Roll No:"+" "+this.Id);
+        System.out.println("Year"+" "+this.year);
+        System.out.println("Room Number"+" "+this.Room);
+        System.out.println("Hostel Fees Status:"+" "+this.Hostel_Fees);
+    }
+}
+class Staff{
+    int Staff_Id;
+    String Staff_Name;
+    int salary;
+
+}
+class Room extends Student{
+    int Capacity;
+    String Type;
+
+}
+public class Main{
+    public static void main(String [] args){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* Input from user to execute */
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        do{
+            System.out.println("1.Add Student");
+            System.out.println("2.View Student");
+            System.out.println("3.Update Student");
+            System.out.println("4.Remove Student");
+            System.out.println("5.Room Allotment");
+            System.out.println("6.Pay Hostel Fees");
+            System.out.println("7.View Staff");
+            System.out.println("8.Exit");
+        }
+        while(n!=8){
+
         }
     }
 }
